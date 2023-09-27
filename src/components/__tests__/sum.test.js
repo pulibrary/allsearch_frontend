bdb7cecb392e1ce090c739d5d9f4a2cfe512b2cd
Max Kadel <mkadel@princeton.edu>
2023-09-27 12:28:28 -0400

    Add Vitest with example test and CircleCI

---
+++ src/components/__tests__/sum.test.js
@@ -0,0 +1,11 @@
+// This is only to have an example for testing,
+// Once we have real examples, this can be removed
+import { test, expect } from "vitest";
+
+function sum(a, b) {
+  return a + b;
+}
+
+test("add 2 numbers", () => {
+  expect(sum(2,3)).toEqual(5);
+});
