7096f1260a745ec2eecd8e26f1153fad471dcc30
Jane Sandberg <js7389@princeton.edu>
2023-09-29 10:29:26 -0700

    honeybadger example

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import HoneybadgerVue from "@honeybadger-io/vue"
+import Honeybadger from "@honeybadger-io/js";
 
-createApp(App).mount("#app");
+const app = createApp(App)
+
+const config = {
+    apiKey: import.meta.env.VITE_HONEYBADGER_API_KEY,
+    environment: "production"
+}
+
+app.use(HoneybadgerVue, config)
+
+app.mount("#app")
+Honeybadger.notify("Testing Honeybadger!");
