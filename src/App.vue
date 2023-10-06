9beb97ddfe4e6a94bebb358f2a0528615aee40b3
Jane Sandberg <js7389@princeton.edu>
2023-10-06 10:55:39 -0700

    i19: Add a basic search form

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SearchBar from './components/SearchBar.vue';
+</script>
 
 <template>
-  <div>
+  <main>
     <h1>Coming soon</h1>
-  </div>
+    <SearchBar></SearchBar>
+  </main>
 </template>
