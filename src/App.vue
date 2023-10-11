d78bb77d8bff14c633b4c29caa683a92e87416fb
Jane Sandberg <js7389@princeton.edu>
2023-10-11 15:29:49 -0700

    Add skip navigation link

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import CatalogResults from './components/CatalogResults.vue';
 import SearchBar from './components/SearchBar.vue';
+import AppHeader from './components/AppHeader.vue'
 </script>
 
 <template>
-  <main>
+  <a class="skip-link" href="#main-content">Skip to main content</a>
+  <AppHeader></AppHeader>
+  <main id="main-content" tabindex="-1">
     <h1>Coming soon</h1>
     <SearchBar></SearchBar>
     <div class="row">
@@ -18,5 +21,14 @@ import SearchBar from './components/SearchBar.vue';
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
+  }
+  a.skip-link {
+    z-index: 25;
+    position: absolute;
+    top: 5px;
+    background: white;
+    padding: 0 8px 8px;
+    border-bottom-right-radius: 3px;
+    color: var(--black);
   }
 </style>
