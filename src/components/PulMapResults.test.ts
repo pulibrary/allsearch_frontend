e9af5c6659268854eeb223abb025232dccdaa90f
Jane Sandberg <js7389@princeton.edu>
2023-10-19 12:53:56 -0700

    i103: remove pulmap component

--- src/components/PulMapResults.test.ts
+++ src/components/PulMapResults.test.ts
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
 import { describe, test, expect, vi, afterEach, beforeEach } from 'vitest';
-import { flushPromises, mount } from '@vue/test-utils';
-import PulmapResults from './PulMapResults.vue';
+import { VueWrapper, flushPromises, mount } from '@vue/test-utils';
 import { SearchService } from '../services/SearchService';
 import { SearchResults } from '../models/SearchResults';
 import PulMapResultsFixtures from '../fixtures/PulMapResultsFixtures';
+import SearchTray from './SearchTray.vue';
+import { SearchScope } from '../enums/SearchScope';
+
+let wrapper: VueWrapper;
+let mock;
 
 describe('PulmapResults component', () => {
   const testResults = new SearchResults(
@@ -18,28 +22,34 @@ describe('PulmapResults component', () => {
   );
 
   beforeEach(() => {
-    const mock = vi.spyOn(SearchService.prototype, 'results');
+    mock = vi.spyOn(SearchService.prototype, 'results');
     mock.mockResolvedValue(testResults);
+    wrapper = mount(SearchTray, {
+      props: {
+        scope: SearchScope.PulMap,
+        resultsPromise: new SearchService().results(
+          SearchScope.PulMap,
+          'searching for robots'
+        ),
+        defaultIcon: 'map'
+      }
+    });
   });
   afterEach(() => {
     vi.restoreAllMocks();
   });
 
   test('it has a heading', () => {
-    const wrapper = mount(PulmapResults);
     expect(wrapper.find('h2').text()).toEqual('Maps and Geographic Data');
   });
   test('heading has a map icon', () => {
-    const wrapper = mount(PulmapResults);
     expect(wrapper.find('h2 span.icon.icon-map').exists()).toBe(true);
   });
   test('it shows three results', async () => {
-    const wrapper = mount(PulmapResults);
     await flushPromises();
     expect(wrapper.findAll('li.document').length).toEqual(3);
   });
   test('the title is in an h3', async () => {
-    const wrapper = mount(PulmapResults);
     await flushPromises();
     const titles = wrapper.findAll('h3');
     expect(titles[0].text()).toEqual('Mushrooms, Average Yield, 2000');
@@ -51,7 +61,6 @@ describe('PulmapResults component', () => {
     );
   });
   test('it shows the formats and their icons', async () => {
-    const wrapper = mount(PulmapResults);
     await flushPromises();
     const results = wrapper.findAll('li.document');
     expect(results[0].text()).toContain('GeoTIFF');
@@ -68,7 +77,6 @@ describe('PulmapResults component', () => {
     ).toBe(true);
   });
   test('the title h3 contains a data-attribute with the id', async () => {
-    const wrapper = mount(PulmapResults);
     await flushPromises();
     const titles = wrapper.findAll('h3');
     expect(titles[0].attributes('data-id')).toEqual('stanford-nz384hp1906');
@@ -76,7 +84,6 @@ describe('PulmapResults component', () => {
     expect(titles[2].attributes('data-id')).toEqual('stanford-zx442cb4791');
   });
   test('it contains a link to more results', async () => {
-    const wrapper = mount(PulmapResults);
     await flushPromises();
     const link = wrapper.find(
       'a[href="https://maps.princeton.edu/catalog?q=mushrooms&search_field=all_fields"]'
@@ -86,13 +93,22 @@ describe('PulmapResults component', () => {
 
   describe('if there are no results', () => {
     beforeEach(() => {
-      const mock = vi.spyOn(SearchService.prototype, 'results');
+      mock = vi.spyOn(SearchService.prototype, 'results');
       mock.mockResolvedValue(
         new SearchResults(0, 'https://pulmap.example.com/zero', [])
       );
+      wrapper = mount(SearchTray, {
+        props: {
+          scope: SearchScope.PulMap,
+          resultsPromise: new SearchService().results(
+            SearchScope.PulMap,
+            'searching for robots'
+          ),
+          defaultIcon: 'map'
+        }
+      });
     });
     test('it shows helpful text', async () => {
-      const wrapper = mount(PulmapResults);
       await flushPromises();
       expect(wrapper.findAll('li').length).toEqual(0);
       expect(wrapper.text()).toContain(
