e25ee6570db6f283719ae212bab819a4781ca847
Ryan Laddusaw <rladdusaw@princeton.edu>
2023-10-25 11:49:50 -0400

    Removed CatalogResults componenent

---
+++ src/components/metadata/CatalogMetadata.test.ts
@@ -0,0 +1,27 @@
+import { beforeEach, describe, expect, test } from "vitest";
+import { mount, VueWrapper } from '@vue/test-utils';
+import CatalogMetadata from './CatalogMetadata.vue'
+import CatalogMetadataFixtures from "../../fixtures/CatalogMetadataFixtures";
+
+describe('CatalogMetadata', () => {
+  let wrapper: VueWrapper;
+  beforeEach(() => {
+    wrapper = mount(CatalogMetadata, {
+      props: {
+        document: CatalogMetadataFixtures.testResult
+      }
+    });
+  });
+
+  test('it shows the library and call number if they exist', async () => {
+    const results = wrapper.findAll('li.access-info');
+    expect(results[0].text()).toContain('Firestone');
+  });
+
+  test('it shows an Available Online link if resource_url exists', async () => {
+    const result = wrapper.findAll('a');
+    expect(result[0].attributes('href')).toEqual(
+      'https://na05.alma.exlibrisgroup.com/view/uresolver/01PRI_INST/openurl?u.ignore_date_coverage=true&portfolio_pid=53763462940006421&Force_direct=true'
+    );
+  });
+});
---
+++ src/fixtures/CatalogMetadataFixtures.ts
@@ -0,0 +1,17 @@
+import { SearchResult } from "../models/SearchResult";
+
+export default {
+  testResult: SearchResult.fromObject({
+    title: 'Potato',
+    publisher: 'Potato company',
+    creator: 'Shrek',
+    id: '123',
+    type: 'Book',
+    url: 'https://catalog.princeton.edu/catalog/99125129988006421',
+    other_fields: {
+      library: 'Firestone',
+      resource_url:
+        'https://na05.alma.exlibrisgroup.com/view/uresolver/01PRI_INST/openurl?u.ignore_date_coverage=true&portfolio_pid=53763462940006421&Force_direct=true'
+    }
+  })
+}
