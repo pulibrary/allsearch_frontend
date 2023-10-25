0af3063fa0a20369f69a2ad39aacb690b58e06bc
Jane Sandberg <js7389@princeton.edu>
2023-10-25 13:07:28 -0700

    Adjust text spacing, especially in the metadata display

--- src/components/InlineBadge.vue
+++ src/components/InlineBadge.vue
@@ -7,8 +7,9 @@
   color: #3e5b0c;
   padding: 4px;
   letter-spacing: 0.32px;
-  margin: 0.25rem;
+  margin: 0.25rem 0.25rem 0.25rem 0;
   border-radius: 3px;
   border: 1px solid var(--gray);
+  display: inline-block;
 }
 </style>
