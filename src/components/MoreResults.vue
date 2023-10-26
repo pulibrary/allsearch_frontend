f98d9e8dc35c7fdf7ac3af8efc6836956158385b
Jane Sandberg <js7389@princeton.edu>
2023-10-26 09:20:25 -0700

    i129: view and refine link: make it a button on focus/hover

--- src/components/MoreResults.vue
+++ src/components/MoreResults.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="more-container">
     <a :href="props.url" class="more-link"
-      >View and refine {{ props.resultCount.toLocaleString() }} results</a
-    >
+      ><span class="underline"
+        >View and refine {{ props.resultCount.toLocaleString() }} results</span
+      >
+      <span class="icon icon-right"></span
+    ></a>
   </div>
 </template>
 <script setup lang="ts">
@@ -25,5 +28,23 @@ const props = defineProps({
 
 a.more-link {
   color: var(--black);
+  text-decoration: none;
+  padding: 7px;
+  margin: 12px 0 7px;
+}
+
+a.more-link:focus,
+a.more-link:hover {
+  border-radius: 4px;
+  color: #fff;
+  background-color: var(--orange);
+}
+
+a.more-link .underline {
+  text-decoration: underline;
+}
+
+a.more-link .icon-right {
+  padding-left: 3px;
 }
 </style>
