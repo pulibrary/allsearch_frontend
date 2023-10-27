37e1f5ca3f14fd9f9afd9224e1c62fb24e45db79
Jane Sandberg <js7389@princeton.edu>
2023-10-27 13:11:28 -0700

    Add ids to trays

--- src/components/TrayTitle.vue
+++ src/components/TrayTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2>
+  <h2 :id="headingId">
     <span v-if="props.icon" :class="iconClass" aria-hidden="true"></span>
     {{ props.heading }}
   </h2>
@@ -8,10 +8,20 @@
 <script setup lang="ts">
 const props = defineProps({
   icon: String,
-  heading: String,
+  heading: {
+    type: String,
+    required: true
+  },
   description: String
 });
 const iconClass = 'icon icon-' + props.icon;
+const headingId = props.heading
+  .toLowerCase()
+  .trim()
+  .replace(/\s/g, '-')
+  // only ASCII letters, digits, '_', and '-' should be used in an id, per
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/id
+  .replace(/[^\w\d-_]/g, '');
 </script>
 <style>
 h3 a {
