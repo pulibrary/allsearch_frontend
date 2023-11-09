3e04610d8a0764166b3c136a3e6d5a07d004c570
Isha Sinha <is4684@princeton.edu>
2023-11-09 13:40:20 -0500

    Modify tests

--- src/components/SearchBar.test.ts
+++ src/components/SearchBar.test.ts
@@ -3,10 +3,6 @@ import { mount } from '@vue/test-utils';
 import SearchBar from './SearchBar.vue';
 
 describe('Searchbar component', () => {
-  // test('it has a label', () => {
-  //   const wrapper = mount(SearchBar);
-  //   expect(wrapper.find('label').exists()).toBe(true);
-  // });
   test('input includes the current search', () => {
     Object.defineProperty(window, 'location', {
       value: new URL('https://allsearch.princeton.edu?q=narwhal')
@@ -14,25 +10,25 @@ describe('Searchbar component', () => {
     const wrapper = mount(SearchBar);
     expect(wrapper.find('input').element.value).toEqual('narwhal');
   });
-  // [
-  //   '"><script>alert("bad")</script>',
-  //   '<script src=example.com></script>',
-  //   `javascript:/*--></title></style></textarea></script></xmp><svg/onload='+/"/+/onmouseover=1/+/[*/[]/+alert(1)//'>`,
-  //   'javascript:alert(String.fromCharCode(88,83,83))',
-  //   "jav ascript:alert('XSS');"
-  // ].forEach(payload => {
-  //   test(
-  //     "unusual search queries don't cause the input's value attribute to end early, payload: " +
-  //       payload,
-  //     () => {
-  //       Object.defineProperty(window, 'location', {
-  //         value: new URL('https://allsearch.princeton.edu?q=' + payload)
-  //       });
-  //       const wrapper = mount(SearchBar);
-  //       expect(wrapper.find('label').exists()).toBe(true);
-  //     }
-  //   );
-  // });
+  [
+    '"><script>alert("bad")</script>',
+    '<script src=example.com></script>',
+    `javascript:/*--></title></style></textarea></script></xmp><svg/onload='+/"/+/onmouseover=1/+/[*/[]/+alert(1)//'>`,
+    'javascript:alert(String.fromCharCode(88,83,83))',
+    "jav ascript:alert('XSS');"
+  ].forEach(payload => {
+    test(
+      "unusual search queries don't cause the input's value attribute to end early, payload: " +
+        payload,
+      () => {
+        Object.defineProperty(window, 'location', {
+          value: new URL('https://allsearch.princeton.edu?q=' + payload)
+        });
+        const wrapper = mount(SearchBar);
+        expect(wrapper.find('.align-searchbar').exists()).toBe(true);
+      }
+    );
+  });
   test('input includes the current search', () => {
     Object.defineProperty(window, 'location', {
       value: new URL('https://allsearch.princeton.edu?q=narwhal')
