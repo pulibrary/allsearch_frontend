6ea51c0464be3462fa380a77e0fecde661765525
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-02-08 14:50:28 -0500

    Skip to section no longer open/closes

--- src/components/JumpToSection.vue
+++ src/components/JumpToSection.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="jump-to-section">
-    <details :open="shouldShowSkipLinks()">
-      <summary>Skip to section</summary>
-      <ul>
-        <div v-for="column of [0, 1, 2]" :key="column" class="column">
-          <template v-for="scope of trayOrder.order[column]" :key="scope">
-            <li v-if="shouldDisplay(scope)">
-              <a :href="getHref(scope)">{{ ScopeTitleMap[scope] }}</a>
-            </li>
-          </template>
-        </div>
-      </ul>
-    </details>
+    <div class="skip-to-section">Skip to section</div>
+    <ul>
+      <div v-for="column of [0, 1, 2]" :key="column" class="column">
+        <template v-for="scope of trayOrder.order[column]" :key="scope">
+          <li v-if="shouldDisplay(scope)">
+            <a :href="getHref(scope)">{{ ScopeTitleMap[scope] }}</a>
+          </li>
+        </template>
+      </div>
+    </ul>
   </div>
 </template>
 <script setup lang="ts">
@@ -38,9 +36,6 @@ function shouldDisplay(scope: SearchScope): boolean {
   }
   return false;
 }
-function shouldShowSkipLinks(): boolean {
-  return window.matchMedia('(min-width: 1280px)').matches;
-}
 const trayOrder = new TrayOrder();
 </script>
 <style scoped>
@@ -55,11 +50,12 @@ ul {
 li {
   width: 80%;
   list-style: none;
+  padding-bottom: 8px;
 
   a:link {
     color: var(--black);
   }
-  
+
   a:visited {
     color: var(--black);
   }
@@ -70,14 +66,13 @@ details {
   padding: 0.5em 0.5em 0;
 }
 
-summary {
+.skip-to-section {
   font-weight: bold;
   margin: -0.5em -0.5em 0;
   padding: 0.5em;
-  cursor: pointer;
 }
 
-details[open] {
+.skip-to-section {
   padding: 0.5em;
 }
 
