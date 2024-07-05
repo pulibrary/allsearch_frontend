67284f25db721f7a5a673ce9465171b2ed7cc18c
Jane Sandberg <js7389@princeton.edu>
2024-07-05 08:29:20 -0700

    [#316] Use LuxLibraryHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,22 +1,11 @@
 <template>
-  <header>
-    <div class="header--primary">
-      <nav class="navbar">
-        <a class="logo" href="https://library.princeton.edu">
-          <img
-            alt="Princeton University Logo"
-            src="/assets/logos/pul-logo-new.svg"
-          />
-        </a>
-        <lux-menu-bar type="main-menu" theme="dark" :menu-items="menu_items" />
-      </nav>
-    </div>
-    <!-- End of head--primary class -->
-  </header>
+  <LuxLibraryHeader type="header" appName="AllSearch">
+    <LuxMenuBar type="main-menu" theme="dark" :menu-items="menu_items" />
+  </LuxLibraryHeader>
 </template>
 
 <script setup lang="ts">
-import { LuxMenuBar } from 'lux-design-system';
+import { LuxLibraryHeader, LuxMenuBar } from 'lux-design-system';
 const menu_items = [
   {
     name: 'Accounts',
@@ -69,51 +58,3 @@ const menu_items = [
   }
 ];
 </script>
-
-<style>
-header {
-  width: 100%;
-  padding: 1rem;
-  height: 8rem;
-  margin: 0em 0em 1em 0em;
-  background-color: var(--gray-100);
-  border-bottom: solid var(--orange-50) 3px;
-}
-
-.header--primary {
-  align-content: center;
-  display: flex;
-  flex-flow: row wrap;
-}
-
-.logo {
-  display: flex;
-  flex-grow: 0;
-  width: 240px;
-}
-
-.navbar {
-  display: flex;
-  flex-flow: row wrap;
-  flex-grow: 1;
-  justify-content: space-between;
-}
-
-header nav.lux-main-menu {
-  /* TODO: Remove this rule and move it to lux */
-  width: auto !important;
-}
-
-/* TODO: handle styling for screens less than 421px; */
-@media (max-width: 645px) {
-  header {
-    height: 10rem;
-  }
-
-  .navbar {
-    display: flex;
-    flex-flow: column wrap;
-    align-items: center;
-  }
-}
-</style>
