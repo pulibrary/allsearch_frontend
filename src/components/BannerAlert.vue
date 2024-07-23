bbeb3e5f7fca6161c2b7dcd4d1e544a64758ce23
Jane Sandberg <js7389@princeton.edu>
2024-07-23 10:27:16 -0700

    [#391] Remember when the user has dismissed the banner
    We use localStorage to remember that a user has dismissed the banner. As
    long as the text doesn't change, we won't display the banner again for
    another week.

--- src/components/BannerAlert.vue
+++ src/components/BannerAlert.vue
@@ -1,12 +1,13 @@
 <template>
   <lux-alert
-    v-if="result?.display_banner"
-    :status="result.alert_status"
-    :autoclear="result.autoclear"
-    :dismissible="result.dismissible"
+    v-if="shouldDisplayBanner()"
+    :status="result?.alert_status"
+    :autoclear="result?.autoclear"
+    :dismissible="result?.dismissible"
+    @dismissed="handleDismissed"
   >
     <!-- nosemgrep javascript.vue.security.audit.xss.templates.avoid-v-html.avoid-v-html -->
-    <span v-html="result.text"></span>
+    <span v-html="result?.text"></span>
   </lux-alert>
 </template>
 <script setup lang="ts">
@@ -21,6 +22,40 @@ const result: Ref<BannerResult | undefined> = ref(undefined);
 
 async function populateResult(): Promise<void> {
   result.value = await bannerPromise;
+}
+
+function handleDismissed() {
+  window.localStorage.setItem(
+    'allsearch-banner-dismissed',
+    JSON.stringify({
+      date: new Date().toISOString().split('T')[0],
+      text: result.value?.text
+    })
+  );
+}
+
+function shouldDisplayBanner(): boolean {
+  if (!result.value) {
+    return false;
+  }
+  const lastDismissed = window.localStorage.getItem(
+    'allsearch-banner-dismissed'
+  );
+  if (lastDismissed) {
+    const parsed = JSON.parse(lastDismissed);
+    const lastDismissedDate = new Date(parsed.date);
+    if (isRecent(lastDismissedDate) && parsed.text === result.value?.text) {
+      return false;
+    }
+  }
+  return result.value?.display_banner || false;
+}
+
+function isRecent(date: Date): boolean {
+  var cutoffDate = new Date();
+  var sevenDaysAgo = cutoffDate.getDate() - 7;
+  cutoffDate.setDate(sevenDaysAgo);
+  return date > cutoffDate;
 }
 
 populateResult();
