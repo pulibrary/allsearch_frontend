5494b75eca876c81588cac00a9dda9f43dd21e00
Christina Chortaria <actspatial@gmail.com>
2025-06-18 13:06:42 -0400

    [TrayContainer]Helps with #506 * Remove margin from h2 * Remove padding/margin from li.document * Add gap in betwewen the document flex-items
    [TrayTitle]
    * Wrap description and h2 in dic class tray-header
    * Set flex attributes to match figma
    * Remove padding from description
    * use only bottom padding in description

--- src/components/TrayTitle.vue
+++ src/components/TrayTitle.vue
@@ -1,9 +1,11 @@
 <template>
-  <h2 :id="headingId">
-    {{ props.heading }}
-  </h2>
-  <div class="description">
-    {{ props.description }}
+  <div class="tray-header">
+    <h2 :id="headingId">
+      {{ props.heading }}
+    </h2>
+    <div class="description">
+      {{ props.description }}
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -22,13 +24,19 @@ const props = defineProps({
 const headingId = IdService.createDomId(props.heading);
 </script>
 <style>
+.tray-header {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+
 h3 a {
   color: light-dark(var(--color-cold-blue), var(--gray-10));
   text-decoration: none;
 }
 
 .description {
-  padding: 3px 0 8px;
+  padding-bottom: 16px;
   border-bottom: solid 2px var(--orange-50);
 }
 </style>
