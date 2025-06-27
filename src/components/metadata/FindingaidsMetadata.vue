46af891d230f1f1173f213432439124e2b612e50
Jane Sandberg <js7389@princeton.edu>
2025-06-27 11:28:46 -0700

    [FindingaidsMetadata] Add show more for the abstract

--- src/components/metadata/FindingaidsMetadata.vue
+++ src/components/metadata/FindingaidsMetadata.vue
@@ -4,14 +4,21 @@
   </li>
   <li v-if="document.description">
     <span class="visually-hidden">Description: </span
-    >{{ StringService.truncate(document.description) }}
+    ><LuxShowMore
+      content-id="findingaids-abstract-{{document.id}}"
+      show-label="Show abstract"
+      hide-label="Hide abstract"
+      :character-limit="characterLimit"
+      :description-id="document.id"
+      >{{ document.description }}</LuxShowMore
+    >
   </li>
 </template>
 
 <script setup lang="ts">
 import { PropType } from 'vue';
 import { SearchResult } from '../../models/SearchResult';
-import { StringService } from '../../services/StringService';
+import { LuxShowMore } from 'lux-design-system';
 
 defineProps({
   document: {
@@ -19,4 +26,6 @@ defineProps({
     required: true
   }
 });
+
+const characterLimit = 150;
 </script>
