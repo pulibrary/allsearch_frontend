d7fb91b4456cdf8c7422676ed14155e5d3f257c3
Christina Chortaria <actspatial@gmail.com>
2026-09-01 14:41:45 -0400

    Update node -> 26. Make tests with localStorage to pass (#779)
    * Update nodejs to 26.8.1
    Update vue/test-utils which removes brace-expansion@^2.0.1
    and resolves https://github.com/pulibrary/allsearch_frontend/security/dependabot/132
    "brace-expansion: DoS via exponential-time expansion of consecutive non-expanding {} groups"
    
    * Update circleci config to use node orb 7.2.0
    Update node version to 26.8.1
    
    * Upgrading to node 26.8 made the window.localStorage unavailable in tests
    Define localStorgae in the test to fix the undefined failure
    Update vitest.config.ts to include the test attribute where we define the environment.
    
    * update afterEach with wrapper?.unmount()
    to prevent mounted components leaking to other tests
    Dont create a spy around setItem since we add a mock
    Use the mock localStorage for getItem and setItem

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    environment: 'happy-dom',
+    include: ['src/components/*.test.ts']
+  },
   resolve: {
     alias: {
       vue: 'vue/dist/vue.runtime.esm-bundler.js'
